ff2536eb3838800b068dd011e8bff8050ba3a537
Caio Molinaro <cmolinaro19@gmail.com>
2024-03-08 17:40:51 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### This repository aims to learn and show how to use AWS Lambda Function to return a JSON Payload
+
+### Project develop following AWS Tutorials
---
+++ src/HelloPersonFunction/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloPersonFunction;
+
+public class Person
+{
+    public string? FirstName { get; init; }
+
+    public string? LastName { get; init; }
+}
